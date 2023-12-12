67a099473acd1d7f97b56a97fefc068ca8492e4f
Antonio Sanchez <titosanxez@gmail.com>
2023-12-12 16:34:41 +0100

    Add LogCache

---
+++ src/main/java/org/dc/LogCache.java
@@ -0,0 +1,54 @@
+package org.dc;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class LogCache {
+    private final Long[] entries;
+    private int startIndex;
+    private int size;
+
+    public LogCache(int maxEntries) {
+        this.entries = new Long[maxEntries];
+        this.startIndex = this.entries.length;
+    }
+
+    public void record(Long orderId) {
+        this.startIndex =
+                (this.startIndex - 1 + this.entries.length) % this.entries.length;
+        if (this.size < this.entries.length) {
+            ++this.size;
+        }
+        this.entries[this.startIndex] = orderId;
+    }
+
+    public Long getAt(int index) {
+        int actualIndex = this.computeIndex(index);
+        return this.entries[actualIndex];
+    }
+
+    private int computeIndex(int index) {
+        return (this.startIndex + index) % this.entries.length;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public void remove(int index) {
+        if (index >= this.size) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        int actualIndex = this.computeIndex(index);
+        for (int i = actualIndex; i > 0; --i) {
+            this.entries[i] = this.entries[i-1];
+        }
+        if (this.startIndex <= actualIndex) {
+            this.startIndex = (this.startIndex + 1) % this.entries.length;
+        }
+        this.size -= 1;
+    }
+
+}
---
+++ src/test/java/org/dc/LogCacheTest.java
@@ -0,0 +1,76 @@
+package org.dc;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LogCacheTest {
+
+    @Test
+    void testFlow() {
+        var logCache = new LogCache(3);
+        var entries = new ArrayList<Long>();
+
+        for (int i = 0; i < 3 ; i++) {
+            entries.add((long) i * 10);
+            logCache.record(entries.get(i));
+        }
+        assertEquals(3, logCache.size());
+        assertEquals(entries.get(2), logCache.getAt(0));
+        assertEquals(entries.get(1), logCache.getAt(1));
+        assertEquals(entries.get(0), logCache.getAt(2));
+        // add items beyond max size
+        entries.add(30L);
+        logCache.record(entries.get(3));
+        assertEquals(3, logCache.size());
+        assertEquals(entries.get(3), logCache.getAt(0));
+        assertEquals(entries.get(2), logCache.getAt(1));
+        assertEquals(entries.get(1), logCache.getAt(2));
+        // repeat
+        logCache.record(40L);
+        assertEquals(3, logCache.size());
+        assertEquals(40L, logCache.getAt(0));
+        assertEquals(entries.get(3), logCache.getAt(1));
+        assertEquals(entries.get(2), logCache.getAt(2));
+        logCache.record(50L);
+        assertEquals(3, logCache.size());
+        assertEquals(50L, logCache.getAt(0));
+        assertEquals(40L, logCache.getAt(1));
+        assertEquals(entries.get(3), logCache.getAt(2));
+    }
+
+    @Test
+    void testRemoval() {
+        var logCache = new LogCache(3);
+        var entries = new ArrayList<Long>();
+
+        for (int i = 0; i < 3 ; i++) {
+            entries.add((long) i * 10);
+            logCache.record(entries.get(i));
+        }
+        assertEquals(3, logCache.size());
+        // remove middle element, past the start index
+        logCache.remove(1);
+        assertEquals(2, logCache.size());
+        assertEquals(entries.get(2), logCache.getAt(0));
+        assertEquals(entries.get(0), logCache.getAt(1));
+        // remove first element on a size < max
+        logCache.remove(0);
+        assertEquals(1, logCache.size());
+        assertEquals(entries.get(0), logCache.getAt(0));
+        // add enough elements to fill up the buffer and overflow by two positions
+        logCache.record(30L);
+        logCache.record(40L);
+        logCache.record(50L);
+        logCache.record(60L);
+        assertEquals(3, logCache.size());
+        assertEquals(60L, logCache.getAt(0));
+        // remove older position
+        logCache.remove(2);
+        assertEquals(2, logCache.size());
+        assertEquals(60L, logCache.getAt(0));
+        assertEquals(50L, logCache.getAt(1));
+    }
+}
